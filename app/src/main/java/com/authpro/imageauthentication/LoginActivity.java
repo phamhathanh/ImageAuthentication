f08fdeb98654df5c95238fe0eb8b19eba09f8383
Rei <phamhathanh130394@gmail.com>
2016-03-05 02:35:47 +0700

    buttons fit horizontally

--- app/src/main/java/com/authpro/imageauthentication/LoginActivity.java
+++ app/src/main/java/com/authpro/imageauthentication/LoginActivity.java
@@ -1,15 +1,20 @@
 package com.authpro.imageauthentication;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.Toast;
-import static junit.framework.Assert.*;
+
+import static junit.framework.Assert.assertTrue;
 
 public class LoginActivity extends Activity
 {
@@ -24,22 +29,62 @@ public class LoginActivity extends Activity
 
     private void setTagForChildren()
     {
+        Resources resources = getResources();
+
+        int columnCountResID = R.integer.gridColumnCount,
+            rowCountResID = R.integer.gridRowCount,
+            columnCount = resources.getInteger(columnCountResID),
+            rowCount = resources.getInteger(rowCountResID);
+
         ViewGroup gridLayout = (ViewGroup)findViewById(R.id.gridLayout);
         int childCount = gridLayout.getChildCount();
+        assertTrue("childCount = " + childCount
+                        + ", columnCount = " + columnCount
+                        + ", rowCount = " + rowCount,
+                childCount == columnCount * rowCount);
 
-        Resources resources = getResources();
-        TypedArray images = resources.obtainTypedArray(R.array.images);
+        int screenWidth = getScreenWidth(),
+        // assuming vertical orientation
+            childWidth = screenWidth / columnCount;
+        // beware of calculation error
 
-        for (int i = 0; i < childCount; i++)
+        TypedArray images = null;
+        try
         {
-            View child = gridLayout.getChildAt(i);
-            child.setTag(i);
+            images = resources.obtainTypedArray(R.array.images);
 
-            int imageID = images.getResourceId(i, 0);
+            for (int i = 0; i < childCount; i++) {
+                View child = gridLayout.getChildAt(i);
+                child.setTag(i);
 
-            assertTrue(child instanceof ImageButton);
-            ((ImageButton)child).setImageResource(imageID);
+                int imageID = images.getResourceId(i, 0);
+
+                assertTrue(child instanceof ImageButton);
+                ImageButton imageButton = (ImageButton)child;
+                imageButton.setImageResource(imageID);
+                imageButton.setMaxWidth(childWidth);
+                imageButton.setMinimumHeight(childWidth);
+            }
         }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        finally
+        {
+            images.recycle();
+        }
+    }
+
+    private int getScreenWidth()
+    {
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+
+        return width;
     }
 
     public void onClick(View view)
