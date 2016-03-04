cd1bb0bae1e638ee0f8c8290e8392f1a73de9f33
Rei <phamhathanh130394@gmail.com>
2016-03-05 00:46:05 +0700

    inserted pictures

--- app/src/main/java/com/authpro/imageauthentication/LoginActivity.java
+++ app/src/main/java/com/authpro/imageauthentication/LoginActivity.java
@@ -1,41 +1,15 @@
 package com.authpro.imageauthentication;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageButton;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
+import static junit.framework.Assert.*;
 
 public class LoginActivity extends Activity
 {
@@ -53,10 +27,18 @@ public class LoginActivity extends Activity
         ViewGroup gridLayout = (ViewGroup)findViewById(R.id.gridLayout);
         int childCount = gridLayout.getChildCount();
 
+        Resources resources = getResources();
+        TypedArray images = resources.obtainTypedArray(R.array.images);
+
         for (int i = 0; i < childCount; i++)
         {
             View child = gridLayout.getChildAt(i);
             child.setTag(i);
+
+            int imageID = images.getResourceId(i, 0);
+
+            assertTrue(child instanceof ImageButton);
+            ((ImageButton)child).setImageResource(imageID);
         }
     }
 
