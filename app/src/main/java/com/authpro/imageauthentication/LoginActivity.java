6be89e9ed80a4086add79b14c3ca1dd24c0f4a23
Rei <phamhathanh130394@gmail.com>
2016-03-07 00:27:18 +0700

    perfect buttons

--- app/src/main/java/com/authpro/imageauthentication/LoginActivity.java
+++ app/src/main/java/com/authpro/imageauthentication/LoginActivity.java
@@ -6,11 +6,13 @@ import android.content.res.TypedArray;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import static junit.framework.Assert.assertTrue;
@@ -35,36 +37,38 @@ public class LoginActivity extends Activity
             columnCount = resources.getInteger(columnCountResID),
             rowCount = resources.getInteger(rowCountResID);
 
-        final ViewGroup gridLayout = (ViewGroup)findViewById(R.id.gridLayout);
-        final int childCount = gridLayout.getChildCount();
-        assertTrue("childCount = " + childCount + ", columnCount = " + columnCount + ", rowCount = " + rowCount, childCount == columnCount * rowCount);
-
-        final int screenPixelWidth = getPixelScreenWidth();
-            // assuming vertical orientation
-
-        final DisplayMetrics metrics = resources.getDisplayMetrics();
-        final float density = metrics.density;
-
-        final int padding = (int)resources.getDimension(R.dimen.imagebutton_margin),
-            childWidth = screenPixelWidth / columnCount;// - (int)Math.ceil(2 * padding * density);
-            // beware of calculation error
+        final ViewGroup gridLayout = (ViewGroup)findViewById(R.id.grid);
+        final int realRowCount = gridLayout.getChildCount();
+        assertTrue("realRowCount = " + realRowCount + ", rowCount = " + rowCount, realRowCount == rowCount);
 
         TypedArray images = null;
         try
         {
             images = resources.obtainTypedArray(R.array.images);
 
-            for (int i = 0; i < childCount; i++) {
+            for (int i = 0; i < rowCount; i++)
+            {
                 final View child = gridLayout.getChildAt(i);
-                child.setTag(i);
 
-                final int imageID = images.getResourceId(i, 0);
+                assertTrue(child instanceof LinearLayout);
+                LinearLayout row = (LinearLayout)child;
+                final int realColumnCount = row.getChildCount();
+                assertTrue("realColumnCount = " + realColumnCount + ", columnCount = " + columnCount, realColumnCount == columnCount);
 
-                assertTrue(child instanceof ImageButton);
-                final ImageButton imageButton = (ImageButton)child;
-                imageButton.setImageResource(imageID);
+                for (int j = 0; j < columnCount; j++)
+                {
+                    final View cell = row.getChildAt(j);
 
-                imageButton.setMaxWidth(childWidth);
+                    assertTrue(cell instanceof ImageButton);
+                    final ImageButton imageButton = (ImageButton)cell;
+
+                    final int index = i * columnCount + j,
+                            imageID = images.getResourceId(index, 0);
+                    if (imageID == 0)
+                        throw new IndexOutOfBoundsException("Index is outside of resources array range.");
+                    imageButton.setTag(index);
+                    imageButton.setImageResource(imageID);
+                }
             }
         }
         catch (Exception ex)
@@ -75,17 +79,6 @@ public class LoginActivity extends Activity
         {
             images.recycle();
         }
-    }
-
-    private int getPixelScreenWidth()
-    {
-        WindowManager windowManager = getWindowManager();
-        Display display = windowManager.getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-
-        return width;
     }
 
     public void onClick(View view)
