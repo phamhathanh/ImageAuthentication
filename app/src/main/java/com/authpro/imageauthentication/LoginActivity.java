cced40a9abdef6d8a36f4fe4404ccb30e97a5328
Rei <phamhathanh130394@gmail.com>
2016-03-08 01:00:25 +0700

    working password input

--- app/src/main/java/com/authpro/imageauthentication/LoginActivity.java
+++ app/src/main/java/com/authpro/imageauthentication/LoginActivity.java
@@ -3,32 +3,37 @@ package com.authpro.imageauthentication;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LoginActivity extends Activity
 {
+    private ArrayList<Integer> input = new ArrayList<>();
+    private TextView textView;
+
+    private final ArrayList<Integer> correctInput = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        setTagForChildren();
+        textView = (TextView)findViewById(R.id.textView);
+        setupImages();
     }
 
-    private void setTagForChildren()
+    private void setupImages()
     {
         final Resources resources = getResources();
 
@@ -37,9 +42,9 @@ public class LoginActivity extends Activity
             columnCount = resources.getInteger(columnCountResID),
             rowCount = resources.getInteger(rowCountResID);
 
-        final ViewGroup gridLayout = (ViewGroup)findViewById(R.id.grid);
+        final ViewGroup gridLayout = (ViewGroup) findViewById(R.id.grid);
         final int realRowCount = gridLayout.getChildCount();
-        assertTrue("realRowCount = " + realRowCount + ", rowCount = " + rowCount, realRowCount == rowCount);
+        assertEquals(realRowCount, rowCount);
 
         TypedArray images = null;
         try
@@ -49,21 +54,20 @@ public class LoginActivity extends Activity
             for (int i = 0; i < rowCount; i++)
             {
                 final View child = gridLayout.getChildAt(i);
-
                 assertTrue(child instanceof LinearLayout);
-                LinearLayout row = (LinearLayout)child;
+                LinearLayout row = (LinearLayout) child;
+
                 final int realColumnCount = row.getChildCount();
-                assertTrue("realColumnCount = " + realColumnCount + ", columnCount = " + columnCount, realColumnCount == columnCount);
+                assertEquals(realColumnCount, columnCount);
 
                 for (int j = 0; j < columnCount; j++)
                 {
                     final View cell = row.getChildAt(j);
-
                     assertTrue(cell instanceof ImageButton);
-                    final ImageButton imageButton = (ImageButton)cell;
+                    final ImageButton imageButton = (ImageButton) cell;
 
                     final int index = i * columnCount + j,
-                            imageID = images.getResourceId(index, 0);
+                        imageID = images.getResourceId(index, 0);
                     if (imageID == 0)
                         throw new IndexOutOfBoundsException("Index is outside of resources array range.");
                     imageButton.setTag(index);
@@ -83,17 +87,50 @@ public class LoginActivity extends Activity
 
     public void onClick(View view)
     {
-        Toast.makeText(getApplicationContext(), "This is the #" + view.getTag() + " button." , Toast.LENGTH_SHORT).show();
+        Object tag = view.getTag();
+        assertTrue(tag instanceof Integer);
+        int index = (int)tag;
+        input.add(index);
+
+        int charCount = textView.length() + 1;
+        assertEquals(charCount, input.size());
+        char[] text = new char[charCount];
+        Arrays.fill(text, '*');
+        textView.setText(new String(text));
     }
 
     public void clear(View view)
     {
-        Toast.makeText(getApplicationContext(), "Cleared." , Toast.LENGTH_SHORT).show();
+        input = new ArrayList<>();
+        textView.setText("");
+
+        Toast.makeText(getApplicationContext(), "Cleared.", Toast.LENGTH_SHORT).show();
+        // Somehow this line is necessary for the TextView to update
     }
 
     public void enter(View view)
     {
-        Toast.makeText(getApplicationContext(), "Entered." , Toast.LENGTH_SHORT).show();
+        String result;
+        if (matches())
+            result = "Success!";
+        else
+            result = "Password mismatched.";
+
+        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean matches()
+    {
+        int size1 = input.size(),
+            size2 = correctInput.size();
+        if (size1 != size2)
+            return false;
+
+        for (int i = 0; i < size1; i++)
+            if (input.get(i) != correctInput.get(i))
+                return false;
+
+        return true;
     }
 }
 
