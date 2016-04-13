ab7555b94c5f8f68467991a7b79c4a239e85b7b6
ThanhPH <phamhathanh130394@gmail.com>
2016-04-13 17:13:04 +0700

    Using EF, scrap unit test.

---
+++ server/ImageAuthentication/Controllers/DevicesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ImageAuthentication.Models;
+
+namespace ImageAuthentication.Controllers
+{
+    public class DevicesController : ApiController
+    {
+        private ImageAuthenticationContext db = new ImageAuthenticationContext();
+
+        // GET: api/Devices
+        public IQueryable<Device> GetDevices()
+        {
+            return db.Devices;
+        }
+
+        // GET: api/Devices/5
+        [ResponseType(typeof(Device))]
+        public async Task<IHttpActionResult> GetDevice(long id)
+        {
+            Device device = await db.Devices.FindAsync(id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(device);
+        }
+
+        // PUT: api/Devices/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutDevice(long id, Device device)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != device.DeviceID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(device).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DeviceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Devices
+        [ResponseType(typeof(Device))]
+        public async Task<IHttpActionResult> PostDevice(Device device)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Devices.Add(device);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = device.DeviceID }, device);
+        }
+
+        // DELETE: api/Devices/5
+        [ResponseType(typeof(Device))]
+        public async Task<IHttpActionResult> DeleteDevice(long id)
+        {
+            Device device = await db.Devices.FindAsync(id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+
+            db.Devices.Remove(device);
+            await db.SaveChangesAsync();
+
+            return Ok(device);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DeviceExists(long id)
+        {
+            return db.Devices.Count(e => e.DeviceID == id) > 0;
+        }
+    }
+}
